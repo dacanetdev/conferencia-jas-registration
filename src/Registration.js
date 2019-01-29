30175da0ef194ea4ff17361bae886ea8582c5313
David Carrillo <david.carrillo@unosquare.com>
2019-01-29 14:52:36 -0800

    Initial commit

---
+++ src/Registration.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+class Registration extends Component {
+  state = {  }
+  render() {
+    return (
+      <div>
+        <Button variant="contained" color="primary">
+          Registrar
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default Registration;
