1424530106332e265ff38fd2da8b72d09379c0ca
David Carrillo <david.carrillo@unosquare.com>
2019-01-29 14:52:05 -0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyD_AnS0NYzCWFb4v_TncpjR_V3liqDeVDE",
+  authDomain: "conferencia-jas-registration.firebaseapp.com",
+  databaseURL: "https://conferencia-jas-registration.firebaseio.com",
+  projectId: "conferencia-jas-registration",
+  storageBucket: "conferencia-jas-registration.appspot.com",
+  messagingSenderId: "396476077474"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
