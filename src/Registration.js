f649d0ff62f31c5694dd541f758af012cd35e4cd
David Carrillo <david_carrillo@hotmail.com>
2019-01-31 07:36:42 -0600

    Updated Registration form

--- src/Registration.js
+++ src/Registration.js
@@ -4,13 +4,18 @@ import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
-import { Button } from '@material-ui/core';
+import { Button, Radio, RadioGroup, FormLabel, FormControlLabel } from '@material-ui/core';
 
 class Registration extends React.Component {
-  state = {
-    firstName: '',
-    lastName: ''
+
+  constructor() {
+    super();
+    this.state = {
+      firstName: '',
+      lastName: ''
+    }
   }
+
 
   handleChange = name => event => {
     this.setState({
@@ -20,97 +25,87 @@ class Registration extends React.Component {
 
   render() {
     return (
-        <React.Fragment>
-          <Card>
-            <CardHeader color="primary" title="Registro" subheader="Proporciona tus datos">
-            </CardHeader>
-            <CardContent>
-              <Grid container spacing={24}>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    id="firstName"
-                    name="firstName"
-                    label="Nombre"
-                    fullWidth
-                    autoComplete="fname"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    id="lastName"
-                    name="lastName"
-                    label="Apellido"
-                    fullWidth
-                    autoComplete="lname"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    id="ward"
-                    name="ward"
-                    label="Barrio"
-                    fullWidth
-                    autoComplete="ward"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField id="stake" name="stake" label="Estaca" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    id="city"
-                    name="city"
-                    label="Ciudad"
-                    fullWidth
-                    autoComplete="billing address-level2"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField id="state" name="state" label="Estado" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    id="email"
-                    name="email"
-                    label="E-mail"
-                    fullWidth
-                    autoComplete="billing email"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    id="cellphone"
-                    name="cellphone"
-                    label="Teléfono"
-                    fullWidth
-                    autoComplete="billing cellphone"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    id="zip"
-                    name="zip"
-                    label="Código Postal"
-                    fullWidth
-                    autoComplete="billing postal-code"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={12}>
-                  <Button variant="contained" color="primary">
-                    Registrar
-                  </Button>
-                </Grid>
+      <React.Fragment>
+        <Card>
+          <CardHeader color="primary" title="Registro" subheader="Proporciona tus datos">
+          </CardHeader>
+          <CardContent>
+            <Grid container spacing={24}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="firstName"
+                  name="firstName"
+                  label="Nombre"
+                  fullWidth
+                  autoComplete="fname"
+                />
               </Grid>
-            </CardContent>
-          </Card>
-        </React.Fragment>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="lastName"
+                  name="lastName"
+                  label="Apellido"
+                  fullWidth
+                  autoComplete="lname"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="ward"
+                  name="ward"
+                  label="Barrio"
+                  fullWidth
+                  autoComplete="ward"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <FormLabel component="legend">Sexo *</FormLabel>
+                <RadioGroup
+                  aria-label="Sexo"
+                  name="sexo"
+                  row="true"
+                  value={this.state.sexo}
+                  onChange={this.handleChange}
+                >
+                  <FormControlLabel value="female" control={<Radio />} label="Femenino" />
+                  <FormControlLabel value="male" control={<Radio />} label="Masculino" />
+                </RadioGroup>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField id="stake" name="stake" label="Estaca" fullWidth />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="email"
+                  name="email"
+                  label="E-mail"
+                  fullWidth
+                  autoComplete="billing email"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  id="cellphone"
+                  name="cellphone"
+                  label="Teléfono"
+                  fullWidth
+                  autoComplete="billing cellphone"
+                />
+              </Grid>
+              <Grid item xs={12} sm={12}>
+                <Button variant="contained" color="primary">
+                  Registrar
+                  </Button>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </React.Fragment>
     );
   }
 }
